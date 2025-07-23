df6e28b3a829cf14e0d550b1eb9b566485000c20
shadtanvir <tahmedshad127@gmail.com>
2025-07-23 21:20:03 +0600

    Added Latest Courses section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Loading from "../components/Loading";
 import Banner from "../components/Banner";
+import CoursesSection from "../components/CoursesSection";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <CoursesSection></CoursesSection>
     </div>
   );
 };
